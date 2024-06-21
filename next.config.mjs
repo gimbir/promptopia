ad933eb6eef16e8f6899bc9dad29a5173ab539e3
gimbir <aerengocmen@gmail.com>
2024-06-21 23:34:18 +0300

    fix next.config

--- next.config.mjs
+++ next.config.mjs
@@ -7,11 +7,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true,
     serverComponentsExternalPackages: ['mongoose']
   },
   images: {
-    domains: ['lh3.googleusercontent.com']
+    remotePatterns: [{ hostname: 'lh3.googleusercontent.com' }]
   },
   webpack(config) {
     config.experiments = {

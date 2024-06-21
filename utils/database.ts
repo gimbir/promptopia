e321bd0a0e5d0ab8a3a118397af3a45ff3f78a7a
gimbir <aerengocmen@gmail.com>
2024-06-21 23:35:10 +0300

    user model and database connections

--- utils/database.ts
+++ utils/database.ts
@@ -2,21 +2,23 @@ import mongoose from 'mongoose';
 
 let isConnected = false; // track the connection
 
+const uri = process.env.MONGODB_URI;
+
 export const connectToDB = async () => {
   mongoose.set('strictQuery', true);
-
+  
   if (isConnected) {
     console.log('MongoDB is already connected');
     return;
   }
-
+  
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      dbName: 'share_prompt',
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
-
+    const clientOptions: mongoose.ConnectOptions = { dbName: 'promptopia'};
+    
+    await mongoose.connect(process.env.MONGODB_URI, clientOptions);
+    // await mongoose.connect(uri, clientOptions);
+    
+    await mongoose.connection.db.admin().command({ ping: 1 });
     isConnected = true;
 
     console.log('MongoDB connected');
